c7aca5e39fc2a5ed88e2918674f6892a63dad6f4
Archishman-Naskar <archishmannskr2005@gmail.com>
2025-10-18 01:56:36 +0530

    Setting Express

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import express from "express";
 
 dotenv.config({
   path:"./.env",
@@ -6,3 +7,18 @@ dotenv.config({
 
 let val=process.env.val;
 console.log("val : "+ val);
+
+const app=express();
+const port=process.env.PORT || 3000;
+
+app.get("/",(req,res)=>{
+  res.send("Hello World!");
+})
+
+app.get("/instagram",(req,res)=>{
+  res.send("This is Instragram.");
+})
+
+app.listen(port,()=>{
+  console.log(`App is runnig on the port ${port}`);
+})
