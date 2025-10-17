0348745f6dd2aefad1f860c9e72b1765eeffd90a
Archishman-Naskar <archishmannskr2005@gmail.com>
2025-10-18 03:01:51 +0530

    Creating app.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-import express from "express";
+import app from "./app.js"
 
 dotenv.config({
   path:"./.env",
@@ -8,16 +8,10 @@ dotenv.config({
 let val=process.env.val;
 console.log("val : "+ val);
 
-const app=express();
+
 const port=process.env.PORT || 3000;
 
-app.get("/",(req,res)=>{
-  res.send("Hello World!");
-})
 
-app.get("/instagram",(req,res)=>{
-  res.send("This is Instragram.");
-})
 
 app.listen(port,()=>{
   console.log(`App is runnig on the port ${port}`);
