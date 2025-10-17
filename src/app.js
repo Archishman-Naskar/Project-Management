ad11dbbcbfc57bbd06816e64646a572d2af1f47d
Archishman-Naskar <archishmannskr2005@gmail.com>
2025-10-18 03:27:14 +0530

    Setting Basic Cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 import express from "express";
+import cors from "cors";
 
 const app=express();
+
+//basic set up
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+//basic cros set up
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://locahost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
 
 app.get("/",(req,res)=>{
   res.send("Hello World!");
