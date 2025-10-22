b0c2ffc755dd7379e188d710173d26dd2f7ae6d6
Archishman-Naskar <archishmannskr2005@gmail.com>
2025-10-23 03:35:42 +0530

    Adding validator, middleware for user registration

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -11,21 +11,27 @@ import {
   // resetForgotPassword,
   // verifyEmail,
 } from "../controllers/auth.controllers.js";
-//import { validate } from "../middlewares/validator.middleware.js";
-// import {
+import { validate } from "../middlewares/validator.middleware.js";
+import {
 //   userChangeCurrentPasswordValidator,
 //   userForgotPasswordValidator,
 //   userLoginValidator,
-//   userRegisterValidator,
+  userRegisterValidator,
 //   userResetForgotPasswordValidator,
-// } from "../validators/index.js";
+} from "../validators/index.js";
 // import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 // unsecured route
-router.route("/register").post(registerUser);//goes when /register is hit it starts executing registerUser
-// router.route("/register").post(userRegisterValidator(), validate, registerUser);
+// router.route("/register").post(registerUser);//goes when /register is hit it starts executing registerUser
+router.route("/register").post(userRegisterValidator(), validate, registerUser);
+                               //checks for validation of data and generates data about
+                                                       //collect all the data and throws error if there is any error present
+                                                                //If no error raised by middleware validate registerUser runs
+                              //notice 
+                              // userRegisterValidator() has vracket but validate and registerUser has not bracket because in there defination they has (req,res,next) but the 
+                              // userRegisterValidator() has nothing so we have to run the method                                 
 // router.route("/login").post(userLoginValidator(), validate, login);
 // router.route("/verify-email/:verificationToken").get(verifyEmail);
 // router.route("/refresh-token").post(refreshAccessToken);
